33155866f24b75d3015d77dacfc39bae2d33b2bf
Lucas Rocha <109543492+DasCaixeta@users.noreply.github.com>
2023-04-15 18:16:15 -0300

    fiz validacao entrada de dados na classe FranquiaControladora

--- src/Controller/FranquiaControladora.java
+++ src/Controller/FranquiaControladora.java
@@ -79,6 +79,7 @@ public class FranquiaControladora {
 
         System.out.println("\nInforme o Id da pessoa que Sera a Dona da Franquia: ");
         int idPessoa = Integer.parseInt(scanner.nextLine());
+        idPessoa = vd.validarINT(idPessoa);
 
         Pessoa pessoa = pessoaDAO.buscaPessoaPorId(idPessoa);
 
@@ -89,6 +90,8 @@ public class FranquiaControladora {
         } else {
             System.out.println("\nInforme o Login de Dono de Franquia: ");
             String loginDonoFranquia = scanner.nextLine();
+            loginDonoFranquia = vd.validaString(loginDonoFranquia);
+            
             //colocar a validacao da entrada de dados nessa linha.
             if (franquiaDAO.verificaSeLoginDonoFranquiaEstaSendoUsado(loginDonoFranquia) == true) {
                 System.out.println("\nLogin Dono De Franquia Ja Esta Sendo Usado!");
@@ -96,6 +99,7 @@ public class FranquiaControladora {
 
                 System.out.println("\nInforme a Senha de Dono de Franquia: ");
                 String senhaDonoFranquia = scanner.nextLine();
+                senhaDonoFranquia = vd.validaString(senhaDonoFranquia);
 
                 Pessoa pessoaDonoFranquia = new Pessoa(pessoa.getNomePessoa(), pessoa.getCpf(), pessoa.getEnderecoPessoa(),
                         pessoa.getTelefonePessoa(), loginDonoFranquia, senhaDonoFranquia, "DonodeFranquia", LocalDateTime.now());
@@ -103,15 +107,19 @@ public class FranquiaControladora {
                 if (pessoaDAO.adicionaPessoa(pessoaDonoFranquia) == true) {
                     System.out.println("\nInforme o Nome da Franquia: ");
                     String nomeFranquia = scanner.nextLine();
+                    nomeFranquia = vd.validaString(nomeFranquia);
 
                     System.out.println("\nInforme o Cnpj da Franquia: ");
                     String cnpjFranquia = scanner.nextLine();
+                    cnpjFranquia = vd.validaString(cnpjFranquia);
 
                     System.out.println("\nInforme a Cidade da Franquia: ");
                     String cidadeFranquia = scanner.nextLine();
+                    cidadeFranquia = vd.validaString(cidadeFranquia);
 
                     System.out.println("\nInforme o Endereco da Franquia: ");
                     String enderecoFranquia = scanner.nextLine();
+                    enderecoFranquia = vd.validaString(enderecoFranquia);
 
                     if (franquiaDAO.verificaSeFranquiaExiste(nomeFranquia, cnpjFranquia) == true) {
                         System.out.println("\nA Franquia Ja Existe");
@@ -147,6 +155,7 @@ public class FranquiaControladora {
                 case 1: {
                     System.out.println("Informe o Novo Nome da Franquia: ");
                     String novoNomeFranquia = scanner.nextLine();
+                    novoNomeFranquia = vd.validaString(novoNomeFranquia);
 
                     if (franquiaDAO.atualizarNomeFranquia(franquia, novoNomeFranquia) == true) {
                         System.out.println("\nNome Da Franquia Atualizado Com Sucesso!");
@@ -160,6 +169,7 @@ public class FranquiaControladora {
                 case 2: {
                     System.out.println("Informe a Nova Cidade da Franquia: ");
                     String novaCidadeFranquia = scanner.nextLine();
+                    novaCidadeFranquia = vd.validaString(novaCidadeFranquia);
 
                     if (franquiaDAO.atualizarCidadeFranquia(franquia, novaCidadeFranquia) == true) {
                         System.out.println("\nCidade Da Franquia Atualizado Com Sucesso!");
@@ -171,6 +181,7 @@ public class FranquiaControladora {
                 case 3: {
                     System.out.println("Informe o Novo Endereco Cidade da Franquia: ");
                     String novoEnderecoFranquia = scanner.nextLine();
+                    novoEnderecoFranquia = vd.validaString(novoEnderecoFranquia);
 
                     if (franquiaDAO.atualizarEnderecoFranquia(franquia, novoEnderecoFranquia) == true) {
                         System.out.println("\nEndereco Da Franquia Atualizado Com Sucesso!");
@@ -183,6 +194,7 @@ public class FranquiaControladora {
                 case 4: {
                     System.out.println("Informe o Novo Login De Dono De Franquia: ");
                     String novoLoginDonoFranquia = scanner.nextLine();
+                    novoLoginDonoFranquia = vd.validaString(novoLoginDonoFranquia);
 
                     if (franquiaDAO.atualizaLoginDonoDeFranquia(franquia, novoLoginDonoFranquia) == true) {
                         System.out.println("\nLogin De Dono De Franquia Atualizado Com Sucesso!");
@@ -196,6 +208,7 @@ public class FranquiaControladora {
                 case 5: {
                     System.out.println("Informe a Nova Senha De Dono De Franquia: ");
                     String novaSenhaDonoFranquia = scanner.nextLine();
+                    novaSenhaDonoFranquia = vd.validaString(novaSenhaDonoFranquia);
 
                     if (franquiaDAO.atualizaSenhaDonoDeFranquia(franquia, novaSenhaDonoFranquia) == true) {
                         System.out.println("\nSenha De Dono De Franquia Atualizado Com Sucesso!");
@@ -208,6 +221,7 @@ public class FranquiaControladora {
                 case 6: {
                     System.out.println("Informe o Novo Telefone De Dono De Franquia: ");
                     String novoTelefoneDonoFranquia = scanner.nextLine();
+                    novoTelefoneDonoFranquia = vd.validaString(novoTelefoneDonoFranquia);
 
                     if (franquiaDAO.atualizaTelefoneDonoDeFranquia(franquia, novoTelefoneDonoFranquia) == true) {
                         System.out.println("\nTelefone De Dono De Franquia Atualizado Com Sucesso!");
@@ -228,6 +242,7 @@ public class FranquiaControladora {
 
         System.out.println("\nInforme o Id da pessoa que Sera Medico: ");
         int idPessoa = Integer.parseInt(scanner.nextLine());
+        idPessoa = vd.validarINT(idPessoa);
 
         Pessoa pessoaEncontrada = pessoaDAO.buscaPessoaPorId(idPessoa);
 
@@ -241,21 +256,26 @@ public class FranquiaControladora {
 
                 System.out.println("\nInforme O Crm do Medico: ");
                 String crm = scanner.nextLine();
+                crm = vd.validaString(crm);
 
                 if (medicoDAO.verificaCrm(crm) == true) {
                     System.out.println("O Crm Informado Ja Esta Sendo Usado.");
                 } else {
                     System.out.println("\nInforme A Especialidade do Medico: ");
                     String medicoEspecialidade = scanner.nextLine();
+                    medicoEspecialidade = vd.validaString(medicoEspecialidade);
 
                     System.out.println("\nInforme O Login De Medico: ");
                     String LoginMedico = scanner.nextLine();
+                    LoginMedico = vd.validaString(LoginMedico);
+                    
                     //colocar a validacao da entrada de dados nessa linha.
                     if (medicoDAO.verificaSeloginEstaSendoUsado(LoginMedico) == true) {
                         System.out.println("\nLogin De Medico Ja Esta Sendo Usado!");
                     } else {
                         System.out.println("\nInforme A Senha De Medico: ");
                         String senhaMedico = scanner.nextLine();
+                        senhaMedico = vd.validaString(senhaMedico);
 
                         Pessoa pessoaMedico = new Pessoa(pessoaEncontrada.getNomePessoa(),
                                 pessoaEncontrada.getCpf(), pessoaEncontrada.getEnderecoPessoa(),
@@ -288,6 +308,7 @@ public class FranquiaControladora {
 
         System.out.println("\nInforme o Id da Pessoa Que Sera Dono De Unidade De Franquia: ");
         int idPessoa = Integer.parseInt(scanner.nextLine());
+        idPessoa = vd.validarINT(idPessoa);
 
         Pessoa pessoaEncontrada = pessoaDAO.buscaPessoaPorId(idPessoa);
 
@@ -299,18 +320,24 @@ public class FranquiaControladora {
             } else {
                 System.out.println("\nInforme a Cidade da Unidade De Franquia: ");
                 String cidadeUnidadeFranquia = scanner.nextLine();
+                cidadeUnidadeFranquia = vd.validaString(cidadeUnidadeFranquia);
 
                 System.out.println("\nInforme o Endereco da Unidade De Franquia: ");
                 String enderecoUnidadeFranquia = scanner.nextLine();
+                enderecoUnidadeFranquia = vd.validaString(enderecoUnidadeFranquia);
 
                 System.out.println("\nInforme O Login Dono De Unidade De Franquia: ");
                 String LoginDonoUnidadeFranquia = scanner.nextLine();
+                LoginDonoUnidadeFranquia = vd.validaString(LoginDonoUnidadeFranquia);
+                
+                
                 //colocar a validacao da entrada de dados nessa linha.
                 if (unidadeFranquiaDAO.verificaSeLoginDonoDeUnidadeFranquiaEstaEmUso(LoginDonoUnidadeFranquia) == true) {
                     System.out.println("\nLogin De Dono Unidade Franquia Ja Esta Sendo Usado!");
                 } else {
                     System.out.println("\nInforme A Senha De Dono De Unidade De Franquia: ");
                     String senhaDonoUnidadeFranquia = scanner.nextLine();
+                    senhaDonoUnidadeFranquia = vd.validaString(senhaDonoUnidadeFranquia);
 
                     Pessoa donoUnidadeFranquia = new Pessoa(pessoaEncontrada.getNomePessoa(),
                             pessoaEncontrada.getCpf(), pessoaEncontrada.getEnderecoPessoa(),
--- src/Controller/ValidacaoEntradaDados.java
+++ src/Controller/ValidacaoEntradaDados.java
@@ -1,25 +1,40 @@
-
 package Controller;
 
 import java.util.Scanner;
 
-
 public class ValidacaoEntradaDados {
-     Scanner scanner = new Scanner(System.in);
-    
-   /*Todos os metodos dessa classe serao Publicos.*/
-    
-    public String validaString(String nome)
-    {
-        while(nome.equals("") || nome.equals(" "))
-        {
+
+    Scanner scanner = new Scanner(System.in);
+
+    /*Todos os metodos dessa classe serao Publicos.*/
+    public String validaString(String nome) {
+        while (nome.equals("") || nome.equals(" ")) {
             System.out.println("entrada incorreta!");
             System.out.println("Informe o Nome da Pessoa: ");
             nome = scanner.nextLine();
 
         }
-         return nome;
+        return nome;
     }
-    
-    
+
+    public int validarINT(int numero) {
+
+        while (numero <= 0) {
+            System.out.println("numero invalido!");
+            System.out.println("Informe outro numero Maior que 0 : ");
+            numero = Integer.parseInt(scanner.nextLine());
+        }
+        return numero;
+    }
+
+    public double validarDoble(double numero) {
+
+        while (numero <= 0) {
+            System.out.println("numero invalido!");
+            System.out.println("Informe outro numero Maior que 0 : ");
+            numero = Integer.parseInt(scanner.nextLine());
+        }
+        return numero;
+    }
+
 }
