16e29f720750dcc65eded4efadd0e16c7b26f185
pp23-if <pedrouberaba23@gmail.com>
2023-04-20 07:57:43 -0300

    fiz o menu titulos de procedimento.

--- src/View/MenuTitulosAdmistrador.java
+++ src/View/MenuTitulosAdmistrador.java
@@ -14,7 +14,9 @@ public class MenuTitulosAdmistrador {
         builder.append("\n0 - Para sair do programa.......: ");
         builder.append("\n1 - Ver Cadastro Admnistrador...: ");
         builder.append("\n2 - Consultas...................: ");
-        builder.append("\n3 - : ");
+        builder.append("\n3 - Procedimentos...............: ");
+        builder.append("\n4 - ...................: ");
+        builder.append("\n5 - ...................: ");
         builder.append("\n\nInforme Opcao...................: ");
 
         System.out.print(builder.toString());
@@ -33,6 +35,23 @@ public class MenuTitulosAdmistrador {
         builder.append("\n3 - Cancelar Consulta.............: ");
         builder.append("\n4 - Remarcar Consulta.............: ");
         builder.append("\n\nInforme Opcao.....................: ");
+
+        System.out.print(builder.toString());
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+    
+      public int menuProcedimentos() {
+
+        StringBuilder builder = new StringBuilder("");
+
+        builder.append("\n============ MENU PROCEDIMENTOS ===========");
+        builder.append("\n0 - Para sair do programa.........: ");
+        builder.append("\n1 - Marcar Procedimento...........: ");
+        builder.append("\n2 - Ver Procedimento Marcado......: ");
+        builder.append("\n3 - Cancelar Procedimento.........: ");
+        builder.append("\n4 - Remarcar Procedimento.........: ");
+        builder.append("\n\nInforme Opcao...................: ");
 
         System.out.print(builder.toString());
 
